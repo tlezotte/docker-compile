8e961d8a24be2549ec38ae1979fb66f81ff88996
Tom LeZotte <tom@lezotte.net>
2022-01-12 14:13:15 -0600

    update wget and git version

--- git/compile.sh
+++ git/compile.sh
@@ -7,7 +7,7 @@
 
 
 # compile git if version set
-wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GIT_VER}.tar.gz 
+wget --no-check-certificate https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GIT_VER}.tar.gz 
 tar xf git-${GIT_VER}.tar.gz
 cd git-${GIT_VER}
 ./configure --prefix=/host/git-${GIT_VER}
